a611e67c58bfc04b87ecb04420ad6e68e8320bfa
AlexandrRust <123_00@ukr.net>
2022-04-23 11:51:32 +0300

    hw-07-finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryImg = makeGalleryItems(galleryItems);
+
+
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryImg);
+
+
+galleryContainer.addEventListener('click', fullImg);
+
+function makeGalleryItems(galleryItems) { 
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return  `<div class="gallery__item">
+                        <a class="gallery__link" href="${original}">
+                            <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                            />
+                        </a>
+                    </div>`;
+        })
+        .join(''); 
+}
+
+
+function fullImg(event) {
+        event.preventDefault()
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const elementData = event.target.dataset.source;
+    return createLightbox(elementData);
+}
+
+function createLightbox(elementData) {
+    const instance = basicLightbox.create(`
+		<img width="1400" height="900" src="${elementData}">
+	`, {
+        onShow: (instance) => {
+            
+            document.addEventListener("keydown", event => {
+                if (event.key === "Escape") {
+
+                    instance.close()
+
+                }
+
+            })
+        }
+    })
+        instance.show()
+};
